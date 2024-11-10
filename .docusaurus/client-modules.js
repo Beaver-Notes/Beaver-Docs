8d2e62a105adf6986344d87107d0faf600ea4415
Daniele Rolli <danielerolli@proton.me>
2024-11-10 18:47:24 +0100

    🛠️ Update docs

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/danielerolli/Documents/GitHub/Beaver-Docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/danielerolli/Documents/GitHub/Beaver-Docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/danielerolli/Documents/GitHub/Beaver-Docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/danielerolli/Documents/GitHub/Beaver-Docs/src/css/custom.css"),
-];
